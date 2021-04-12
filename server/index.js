6cb0902874d7726715307aec519954f96ac21693
liori142 <liorimazgi@gmail.com>
2021-04-12 16:15:17 +0300

    a

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-const bodyParser = require('body-parser')
 const cors = require('cors')
 const express = require('express')
 const app = express()
@@ -9,8 +8,8 @@ const router = require('./routes/movieRouter')
 const path = require('path')
 
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
